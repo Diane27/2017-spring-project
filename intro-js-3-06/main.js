f881600bb59c1168dba24dc13c8544369650a89d
oshaw <oscar.shaw@yahoo.com>
2017-03-06 18:37:19 -0800

    Upload submission for intro-js-3-06

--- intro-js-3-06/main.js
+++ intro-js-3-06/main.js
@@ -1,10 +1,12 @@
-var number;
-var input;
-var button;
-var output;
+var number = Math.floor(Math.random() * 10);
+var input = document.getElementById("guess-input");
+var button = document.getElementById("guess-button");
+var output = document.getElementById("output");
 
 function makeGuess () {
-
+	
+	if (parseInt(input.value, 10) === number) output.textContent = "Correct";
+	else output.textContent = "";
 }
 
 button.addEventListener('click', makeGuess)
